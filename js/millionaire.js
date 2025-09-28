430c3ab46428703138401c1e78fea1d4070347fb
Cqoicebordel <Cqoicebordel@users.noreply.github.com>
2025-09-28 16:35:06 +0200

    Features and adaptations
    Adds :
    * Volume level
    * Volume memory through cookie
    * Ability to jump to a question
    * Display author name
    * Longer time before showing the right answer (scale with level)
    * Freeze UI when lost
    
    Adapts :
    * Change the way right and wrong answers are shown
    * How the jokers are handled
    * Money formatting

--- js/millionaire.js
+++ js/millionaire.js
@@ -36,14 +36,53 @@ startSound = function(id, loop) {
 	soundHandle.play();
 }
 
+stopSound = function(id) {
+	soundHandle = document.getElementById(id);
+	soundHandle.pause();
+}
+
+
+function sleep(num) {
+	let now = new Date();
+	let stop = now.getTime() + num;
+	while(true) {
+		now = new Date();
+		if(now.getTime() > stop) return;
+	}
+}
+
+
+function setCookie(cname, cvalue) {
+	document.cookie = cname + "=" + cvalue;
+}
+
+function getCookie(cname) {
+	let name = cname + "=";
+	let decodedCookie = decodeURIComponent(document.cookie);
+	let ca = decodedCookie.split(';');
+	for(let i = 0; i <ca.length; i++) {
+		let c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
+
 /**
 * The View Model that represents one game of
 * Who Wants to Be a Millionaire.
 * 
 * @param data the question bank to use
 */
-var MillionaireModel = function(data) {
+var MillionaireModel = function(index, data) {
 	var self = this;
+	
+	this.questionnaire = index;
 
 	// The 15 questions of this game
     this.questions = data.questions;
@@ -60,10 +99,16 @@ var MillionaireModel = function(data) {
 
  	// The three options the user can use to 
  	// attempt to answer a question (1 use each)
- 	this.usedFifty = new ko.observable(false);
- 	this.usedPhone = new ko.observable(false);
- 	this.usedAudience = new ko.observable(false);
+ 	this.usedFifty = false;
+ 	this.usedPhone = false;
+ 	this.usedAudience = false;
+	
+	this.perdu = false;
 
+	self.getQuestionnaire = function() {
+ 		return self.questionnaire + 1;
+ 	}
+	
  	// Grabs the question text of the current question
  	self.getQuestionText = function() {
  		return self.questions[self.level() - 1].question;
@@ -74,65 +119,108 @@ var MillionaireModel = function(data) {
  	self.getAnswerText = function(index) {
  		return self.questions[self.level() - 1].content[index];
  	}
+ 	
+ 	self.getAuthor = function() {
+ 		if(self.questions[self.level() - 1].disclose){
+			return "Par "+self.questions[self.level() - 1].pseudo;
+		}else{
+			return "";
+		}
+ 	}
+ 	
+ 	$("#goq").click(function() {
+		var index = parseInt($('#question-number').val());
+		self.level(index);
+		$(".answer").removeClass("qselected");
+		$("#answer-one").show();
+		$("#answer-two").show();
+		$("#answer-three").show();
+		$("#answer-four").show();
+	});
 
  	// Uses the fifty-fifty option of the user
  	self.fifty = function(item, event) {
- 		if(self.transitioning)
- 			return;
- 		$(event.target).fadeOut('slow');
- 		var correct = this.questions[self.level() - 1].correct;
- 		var first = (correct + 1) % 4;
- 		var second = (first + 1) % 4;
- 		if(first == 0 || second == 0) {
- 			$("#answer-one").fadeOut('slow');
- 		}
- 		if(first == 1 || second == 1) {
- 			$("#answer-two").fadeOut('slow');
- 		}
- 		if(first == 2 || second == 2) {
- 			$("#answer-three").fadeOut('slow');
- 		}
- 		if(first == 3 || second == 3) {
- 			$("#answer-four").fadeOut('slow');
- 		}
+		if(!self.perdu && !self.usedFifty){
+			if(self.transitioning)
+				return;
+			self.usedFifty = true;
+			sleep(200);
+			$(event.target).css("background-image", "url('img/jokers/50-used.png')");
+			startSound('50', false);
+			var correct = this.questions[self.level() - 1].correct;
+			var first = (correct + 1) % 4;
+			var second = (first + 1) % 4;
+			if(first == 0 || second == 0) {
+				$("#answer-one").css("visibility", "hidden");
+			}
+			if(first == 1 || second == 1) {
+				$("#answer-two").css("visibility", "hidden");
+			}
+			if(first == 2 || second == 2) {
+				$("#answer-three").css("visibility", "hidden");
+			}
+			if(first == 3 || second == 3) {
+				$("#answer-four").css("visibility", "hidden");
+			}
+		}
  	}
 
  	// Fades out an option used if possible
- 	self.fadeOutOption = function(item, event) {
- 		if(self.transitioning)
+ 	self.fadeOutOptionPhone = function(item, event) {
+ 		if(self.transitioning || self.perdu)
  			return;
- 		$(event.target).fadeOut('slow');
+ 		$(event.target).css("background-image", "url('img/jokers/phone-used.png')");
+ 	}
+ 	
+ 	// Fades out an option used if possible
+ 	self.fadeOutOptionPublic = function(item, event) {
+ 		if(self.transitioning || self.perdu)
+ 			return;
+ 		$(event.target).css("background-image", "url('img/jokers/public-used.png')");
  	}
 
  	// Attempts to answer the question with the specified
  	// answer index (0-3) from a click event of elm
  	self.answerQuestion = function(index, elm) {
- 		if(self.transitioning)
- 			return;
- 		self.transitioning = true;
- 		if(self.questions[self.level() - 1].correct == index) {
- 			self.rightAnswer(elm);
- 		} else {
- 			self.wrongAnswer(elm);
- 		}
+ 		if(!self.perdu){
+			if(!$("#" + elm).hasClass("qselected")){
+				$(".answer").removeClass("qselected");
+				$("#" + elm).addClass("qselected");
+			}else{
+				if(self.transitioning)
+					return;
+				self.transitioning = true;
+				if(self.questions[self.level() - 1].correct == index) {
+					self.rightAnswer(elm);
+				} else {
+					self.wrongAnswer(elm);
+				}
+			}
+		}
  	}
 
  	// Executes the proceedure of a correct answer guess, moving
  	// the player to the next level (or winning the game if all
  	// levels have been completed)
  	self.rightAnswer = function(elm) {
- 		$("#" + elm).slideUp('slow', function() {
- 			startSound('rightsound', false);
- 			$("#" + elm).css('background', 'green').slideDown('slow', function() {
+ 		$("#" + elm).fadeOut('slow', function() {
+			$(".answer").removeClass("qselected");
+			sleep(self.level()*200);
+			if(self.level() + 1 <= 15){
+				startSound('rightsound', false);
+			}
+ 			$("#" + elm).addClass("qtrue").fadeIn(self.level()*200+100, function() {
+				$(".answer").css("visibility", "visible");
  				self.money($(".active").data('amt'));
  				if(self.level() + 1 > 15) {
-	 				$("#game").fadeOut('slow', function() {
-	 					$("#game-over").html('You Win!');
-	 					$("#game-over").fadeIn('slow');
-	 				});
+					stopSound('background');
+					startSound('millionwin', false);
+					sleep(2000);
+					$("#gagne").fadeIn(10000);
  				} else {
+					sleep(3000);
  					self.level(self.level() + 1);
- 					$("#" + elm).css('background', 'none');
+ 					$("#" + elm).removeClass("qtrue");
 			 		$("#answer-one").show();
 			 		$("#answer-two").show();
 			 		$("#answer-three").show();
@@ -140,26 +228,44 @@ var MillionaireModel = function(data) {
 			 		self.transitioning = false;
  				}
  			});
+			
  		});
  	}
 
  	// Executes the proceedure of guessing incorrectly, losing the game.
  	self.wrongAnswer = function(elm) {
- 		$("#" + elm).slideUp('slow', function() {
+		self.perdu = true;
+ 		$("#" + elm).fadeOut('slow', function() {
+			$(".answer").removeClass("qselected");
  			startSound('wrongsound', false);
- 			$("#" + elm).css('background', 'red').slideDown('slow', function() {
- 				$("#game").fadeOut('slow', function() {
- 					$("#game-over").html('Game Over!');
- 					$("#game-over").fadeIn('slow');
- 					self.transitioning = false;
- 				});
+			if(self.level()<5){
+				self.money(0);
+			}else if(self.level()<10){
+				self.money(1000);
+			}else{
+				self.money(24000);
+			}
+			stopSound('background');
+			self.transitioning = false;
+			$("#" + elm).addClass("qfalse").fadeIn('slow', function() {
+ 			//$("#" + elm).css('background', 'red').fadeIn('slow', function() {
+				sleep(2000);
+				$(".answer").eq(self.questions[self.level() - 1].correct).addClass("qtrue");
+				if(self.level()<5){
+					$("#perdu-palier0").fadeIn(5000);
+				}else if(self.level()<10){
+					$("#perdu-palier1").fadeIn(5000);
+				}else{
+					$("#perdu-palier2").fadeIn(5000);
+				}
  			});
  		});
  	}
 
  	// Gets the money formatted string of the current won amount of money.
  	self.formatMoney = function() {
-	    return self.money().money(2, '.', ',');
+		// money(decimalNumbers, decimalSeparator, ThousandsSeparator)
+	    return self.money().money(0, '', ' ');;
 	}
 };
 
@@ -174,11 +280,27 @@ $(document).ready(function() {
 		$("#pre-start").show();
 		$("#start").click(function() {
 			var index = $('#problem-set').find(":selected").val() - 1;
-			ko.applyBindings(new MillionaireModel(data.games[index]));
+			ko.applyBindings(new MillionaireModel(index, data.games[index]));
 			$("#pre-start").fadeOut('slow', function() {
 				startSound('background', true);
 				$("#game").fadeIn('slow');
 			});
 		});
 	});
+	let volume = document.getElementById('volume-slider');
+	volume.addEventListener("change", function(e) {
+		setCookie("volume", e.currentTarget.value);
+		var audios = document.getElementsByTagName("audio");
+		for(const audio of audios){
+			audio.volume = e.currentTarget.value / 100;
+		}		
+	});
+	let vol = getCookie("volume");
+	if(vol != ""){
+		volume.value = vol;
+		var audios = document.getElementsByTagName("audio");
+		for(const audio of audios){
+			audio.volume = vol / 100;
+		}
+	}
 });
